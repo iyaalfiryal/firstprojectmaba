4f3462d2fd82a5c49d5a6645e2afc939fcbe2a12
iyaalfiryal <fifihuwaida@gmail.com>
2021-11-09 11:23:50 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/book/create', [BookController::class, 'createBook']);
+
+Route::get('/book/get/{id}', [BookController::class, 'showBookById']);
+
+Route::get('/book/get', [BookController::class, 'showAllBooks']);
+Route::post('/book/update{id}', [BookController::class, 'updateBook']);
+Route::delete('/book/delete{id}', [BookController::class, 'deleteBook']);
